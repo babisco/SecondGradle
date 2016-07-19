5c5910ecf319de0303077cac3d80bd7b0d41ddf8
java <java>
2016-07-19 17:15:11 +0900

    labeling change

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -2,10 +2,10 @@ package com.hybrid;
 
 public class MainApplication {
 	public static void main (String[] args) {
-		System.out.println("====================");
+		System.out.println("++++++++++++++++++++++");
 		for (int i=0; int<10; i++) {
-		System.out.println("This is SecondGradle");
+		System.out.println("This is SecondGradle !");
 		}
-		System.out.println("====================");
+		System.out.println("=====================");
 	}
 }
