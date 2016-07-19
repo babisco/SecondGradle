db0fad094e16e91562e2580ec1f69004a1100680
java <java>
2016-07-19 17:12:23 +0900

    count change for hello

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -3,7 +3,9 @@ package com.hybrid;
 public class MainApplication {
 	public static void main (String[] args) {
 		System.out.println("====================");
+		for (int i=0; int<10; i++) {
 		System.out.println("This is SecondGradle");
+		}
 		System.out.println("====================");
 	}
 }
